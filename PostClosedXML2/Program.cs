5a9ad7cf4c59b3ae99b54576a15dfd082d09ffe3
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-10-30 13:14:52 +0100

    Genero table

--- PostClosedXML2/Program.cs
+++ PostClosedXML2/Program.cs
@@ -1,10 +1,25 @@
 ﻿using ClosedXML.Excel;
 using System;
+using System.Collections.Generic;
 
 namespace PostClosedXML2
 {
   class Program
   {
+    static IEnumerable<XLColor> GetColors()
+    {
+      yield return XLColor.Red;
+      yield return XLColor.Amber;
+      yield return XLColor.AppleGreen;
+      yield return XLColor.AtomicTangerine;
+      yield return XLColor.BallBlue;
+      yield return XLColor.Bittersweet;
+      yield return XLColor.CalPolyPomonaGreen;
+      yield return XLColor.CosmicLatte;
+      yield return XLColor.DimGray;
+    }
+
+
     static void Main(string[] args)
     {
       using (var workbook = new XLWorkbook())
@@ -15,17 +30,35 @@ namespace PostClosedXML2
         worksheet.Cell("A1").Value = "Nombre";
         worksheet.Cell("B1").Value = "Color";
         //Le damos el formato a la cabecera        
-        worksheet.Range("A1:B1").Style.Border.BottomBorder = XLBorderStyleValues.Thick;
-        worksheet.Range("A1:B1").Style.Border.TopBorder = XLBorderStyleValues.Thick;
-        worksheet.Range("A1:B1").Style.Border.LeftBorder = XLBorderStyleValues.Thick;
-        worksheet.Range("A1:B1").Style.Border.RightBorder = XLBorderStyleValues.Thick;
-        worksheet.Range("A1:B1").Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-        worksheet.Range("A1:B1").Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
-        worksheet.Range("A1:B1").Style.Font.FontSize = 8;
+        var rango = worksheet.Range("A1:B1");
+        rango.Style.Border.BottomBorder = XLBorderStyleValues.Thick;
+        rango.Style.Border.TopBorder = XLBorderStyleValues.Thick;
+        rango.Style.Border.LeftBorder = XLBorderStyleValues.Thick;
+        rango.Style.Border.RightBorder = XLBorderStyleValues.Thick;
+        rango.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+        rango.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+        rango.Style.Font.FontSize = 14;
+        rango.Style.Fill.BackgroundColor = XLColor.AliceBlue;
+        worksheet.Columns(1, 2).AdjustToContents();
 
+        int nRow = 2;
+        //Genero la tabla de colores
+        foreach(var color in GetColors())
+        {
+          worksheet.Cell(nRow, 1).Value = color.ToString();
+          worksheet.Cell(nRow, 2).Style.Fill.BackgroundColor = color;
+          nRow++;
+        }
 
+        //Aplico los formatos
+        rango = worksheet.Range(2, 1, nRow-1, 2);
+        rango.Style.Border.BottomBorder = XLBorderStyleValues.Medium;
+        rango.Style.Border.TopBorder = XLBorderStyleValues.Medium;
+        rango.Style.Border.LeftBorder = XLBorderStyleValues.Medium;
+        rango.Style.Border.RightBorder = XLBorderStyleValues.Medium;
+        rango.Style.Border.InsideBorder = XLBorderStyleValues.Medium;
 
-        workbook.SaveAs("HelloWorld.xlsx");
+        workbook.SaveAs("CellFormating.xlsx");
       }
     }
   }
